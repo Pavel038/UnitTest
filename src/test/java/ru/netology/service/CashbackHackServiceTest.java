9727e25b6c433e37efa8f59b5423766bd6778291
Pavel <zenzin92@mail.ru>
2023-03-02 19:09:13 +0700

    second commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,49 +1,51 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void remain() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 999;
+        void remain() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 999;
 
-        int actual = cashbackHackService.remain(amount);
-        int expected = 1;
-        assertEquals(expected,actual);
+            int actual = cashbackHackService.remain(amount);
+            int expected = 1;
+            assertEquals(actual, expected);
 
+        }
+
+        @Test
+        void minValue() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 0;
+
+            int actual = cashbackHackService.remain(amount);
+            int expected = 1000;
+            assertEquals(actual, expected);
+
+        }
+        @Test
+        void negativeMeaning() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = -1;
+
+            int actual = cashbackHackService.remain(amount);
+            int expected = 1001;
+            assertEquals(actual, expected);
+
+        }
+        @Test
+        void equalValue() {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 1000;
+
+            int actual = cashbackHackService.remain(amount);
+            int expected = 1000;
+            assertEquals(actual, expected);
+
+        }
     }
-    @Test
-    void minValue() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 0;
 
-        int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
-        assertEquals(expected,actual);
-
-    }
-    @Test
-    void negativeMeaning() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = -1;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 1001;
-        assertEquals(expected,actual);
-
-    }
-    @Test
-    void equalValue() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
-        assertEquals(expected,actual);
-
-    }
-}
