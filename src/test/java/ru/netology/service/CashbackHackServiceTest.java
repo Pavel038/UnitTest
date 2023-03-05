b7083390206c0e6edcee018b9460fe513e3fab36
Pavel038 <98365181+Pavel038@users.noreply.github.com>
2023-03-05 13:39:36 +0700

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -46,7 +46,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(expected, actual);
 
     }
